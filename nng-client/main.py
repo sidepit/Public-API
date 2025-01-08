28c90116ae979dec407187ec7a8afc0049776ebd
yehoste <yehoshuaste@gmail.com>
2025-01-08 18:08:17 +0200

    created a new main file

---
+++ nng-client/main.py
@@ -0,0 +1,35 @@
+from constants import PROTOCOL, ADDRESS, CLIENT_PORT
+from sidepit_nng_client import SidepitClient
+
+def main() -> None:
+    server_address = f"{PROTOCOL}{ADDRESS}:{CLIENT_PORT}"
+    client = SidepitClient(server_address)
+
+    # Example usage
+    client.send_new_order(
+        side=True,
+        size=10,
+        price=100,
+        symbol="BTCUSD",
+        user_id=b"user_id",
+        user_signature=b"user_signature",
+    )
+    client.send_cancel_order(
+        order_id=b"order_id",
+        user_id=b"user_id",
+        user_signature=b"user_signature",
+    )
+    client.send_auction_bid(
+        epoch=1234567890,
+        hash_value="hash_value",
+        ordering_salt="ordering_salt_value",
+        bid=500,
+        user_id=b"user_id",
+        user_signature=b"user_signature",
+    )
+
+    client.close_connection()
+
+
+if __name__ == "__main__":
+    main()
